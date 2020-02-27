cc14a2b4df0c8621d9af78738693f0d302ce4bbb
alvarodelvalle <alvaro.delvalle2@gmail.com>
2020-02-27 12:07:09 -0600

    fixed  Did you mean to define argument config? If so, use the equals sign to assign it a value.

--- ci-runners/remote-state-lookup.tf
+++ ci-runners/remote-state-lookup.tf
@@ -12,7 +12,7 @@ variable "state_file_key" {
 
 data "terraform_remote_state" "vpc" {
   backend = "s3"
-  config {
+  config = {
     region = var.terraform_state_aws_region
     bucket = var.terraform_state_s3_bucket
     key    = var.state_file_key
